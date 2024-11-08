500029c492329b80a6bf74654e41891926a60025
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-11-08 16:47:51 +0300

    Создали нужные модели

--- Themes_15_18/JournalWeb/articles/models.py
+++ Themes_15_18/JournalWeb/articles/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия автора")
+    first_name = models.CharField(max_length=100, verbose_name="Имя автора")    
+    date_of_birth = models.DateField(verbose_name="Дата рождения", null=True, blank=True)   
+    class Meta:
+        abstract = True 
+
+    def __str__(self):
+        return self.last_name      
+
+
+class Author(Person):
+    pass
+
+
+class Language(models.Model):
+    name = models.CharField( help_text="Введите язык", verbose_name="Язвк статьи",
+                            max_length=100, null=True, blank=True)
+    
+    class Meta:
+        verbose_name = "Язык"
+        verbose_name_plural = "Языки"     
+
+    def __str__(self):
+        return self.name  
+    
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название статьи")
+    content = models.TextField(verbose_name="Содержание статьи")
+    language = models.ForeignKey(Language, verbose_name="Язык статьи", on_delete=models.CASCADE)
+    date_published = models.DateField(verbose_name="Дата публикации")
+    author = models.ForeignKey(Author, verbose_name="Автор", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+    def __str__(self):
+        return self.title
+
+    
